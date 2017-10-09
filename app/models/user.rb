cb46226a43a485d425611683c8311e360e3c6d9a
Antoni Pstraś <antek.pstras@gmail.com>
2017-10-08 23:32:53 -0400

    Added basic model specs

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   has_secure_password
-  has_many :quizzes
+  has_many :quizzes, dependent: :destroy
   has_many :friendships
   has_many :friends, through: :friendships
   has_many :inverse_friendships, foreign_key: "friend_id", class_name: 'Friendship'
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should have_many(:quizzes).dependent(:destroy) }
+  it { should have_many(:friendships) }
 end
