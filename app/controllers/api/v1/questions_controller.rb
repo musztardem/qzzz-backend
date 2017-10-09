06205e62e2307d5d19addca36847def23608743d
Antoni Pstraś <antek.pstras@gmail.com>
2017-10-09 14:27:56 -0400

    Added data to seed.rb

--- app/controllers/api/v1/questions_controller.rb
+++ app/controllers/api/v1/questions_controller.rb
@@ -1,2 +1,13 @@
 class Api::V1::QuestionsController < ApplicationController
+  def index
+  end
+
+  def create
+  end
+
+  def update
+  end
+
+  def destroy
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,124 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+
+users = [
+  { username: 'janek', email: 'janek@email.com', password: 'plokplok', password_confirmation: 'plokplok' },
+  { username: 'tomek', email: 'tomek@email.com', password: 'plokplok', password_confirmation: 'plokplok' },
+  { username: 'romek', email: 'romek@email.com', password: 'plokplok', password_confirmation: 'plokplok' },
+]
+
+users.each { |u| User.create!(u) }
+puts "Users created..."
+
+Friendship.create!(user: User.first, friend: User.last)
+puts "Friendship created..."
+
+quizzes = [
+  {
+    title: 'Musicians',
+    description: 'Quiz about famous musicians!',
+    questions: [
+      {
+        content: 'Tom Araya is singer of which band?',
+        answers: [
+          {
+            content: 'Slayer',
+            correct: true
+          },
+          {
+            content: 'Metallica',
+            correct: false
+          },
+          {
+            content: 'Anthrax',
+            correct: false
+          },
+          {
+            content: 'Testament',
+            correct: false
+          },
+        ]
+      },
+      {
+        content: 'Which of the bands is NOT in the big 4?',
+        answers: [
+          {
+            content: 'Slayer',
+            correct: false
+          },
+          {
+            content: 'Metallica',
+            correct: false
+          },
+          {
+            content: 'Anthrax',
+            correct: false
+          },
+          {
+            content: 'Testament',
+            correct: true
+          },
+        ]
+      }
+    ]
+  },
+  {
+    title: 'Programming',
+    description: 'Quiz about programming!',
+    questions: [
+      {
+        content: 'Which programming language is Ruby?',
+        answers: [
+          {
+            content: 'Ruby',
+            correct: true
+          },
+          {
+            content: 'Java',
+            correct: false
+          },
+          {
+            content: 'JavaScript',
+            correct: false
+          },
+          {
+            content: 'Perl',
+            correct: false
+          }
+        ]
+      },
+      {
+        content: 'Which programming language has Ruby in logo?',
+        answers: [
+          {
+            content: 'Ruby',
+            correct: true
+          },
+          {
+            content: 'Java',
+            correct: false
+          },
+          {
+            content: 'JavaScript',
+            correct: false
+          },
+          {
+            content: 'Perl',
+            correct: false
+          }
+        ]
+      },
+    ]
+  }
+]
+
+quizzes.each do |quiz|
+  qz = Quiz.create(user: User.last, title: quiz[:title], description: quiz[:description])
+  questions = quiz[:questions]
+  questions.each do |question|
+    qs = Question.create!(quiz: qz, content: question[:content])
+    answers = question[:answers]
+    answers.each do |answer|
+      Answer.create(question: qs, content: answer[:content], correct: answer[:correct])
+    end
+  end
+end
+puts "Quizzes created..."
