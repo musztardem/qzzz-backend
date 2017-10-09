5805023262a03ac02b94c2df6078c614af795adc
Antoni Pstraś <antek.pstras@gmail.com>
2017-10-09 16:54:19 -0400

    Implemented specs for questions

--- spec/requests/api/v1/questions_spec.rb
+++ spec/requests/api/v1/questions_spec.rb
@@ -5,41 +5,81 @@ RSpec.describe 'API/V1/Questions', type: :request do
   let!(:headers) { valid_headers }
   let!(:quiz) { create :quiz }
   let!(:questions) { create_list(:question, 2, quiz: quiz) }
-
-  before do
-    create_list(:answer, 4, questions.first)
-    create_list(:answer, 4, questions.last)
-  end
+  let(:answers_one) { create_list(:answer, 4, questions.first) }
+  let(:answers_two) { create_list(:answer, 4, questions.last) }
 
   describe 'GET /quizzes/{:quiz_id}/questions' do
-    it 'returns status code 200'
-    it 'returns quiz questions'
+    before { get "/api/v1/quizzes/#{quiz.id}/questions", headers: headers }
+
+    it 'returns status code 200' do
+      expect(response).to have_http_status(200)
+    end
+
+    it 'returns quiz questions' do
+      expect(json['questions'].size).to eq 2
+    end
   end
 
   describe 'POST /quizzes/{:quiz_id}/questions' do
     context 'valid params' do
-      it 'returns status code 201'
-      it 'returns success message'
+      let!(:valid_params) { attributes_for(:question) }
+      before { post "/api/v1/quizzes/#{quiz.id}/questions", params: valid_params, headers: headers }
+
+      it 'returns status code 201' do
+        expect(response).to have_http_status(:created)
+      end
+
+      it 'returns success message' do
+        expect(json['message']).to match(/Question was successfully created/)
+      end
     end
 
     context 'invalid params' do
-      it 'returns status code 422'
-      it 'returns error messages'
+      let!(:invalid_params) { attributes_for(:question).except(:content) }
+      before { post "api/v1/quizzes/#{quiz.id}/questions", params: valid_params, headers: headers }
+
+      it 'returns status code 422' do
+        expect(response).to have_http_status(:unprocessable_entity)
+      end
+
+      it 'returns error messages' do
+        expect(json['messages'].size).to eq 1
+      end
     end
   end
 
   describe 'GET /quizzes/{:quiz_id}/questions/{:id}' do
-    it 'returns status code 200'
-    it 'returns question'
-    it 'returns question answers'
+    before { get "/api/v1/quizzes/#{quiz.id}/questions/#{questions.first.id}", headers: headers }
+
+    it 'returns status code 200' do
+      expect(response).to have_http_status(200)
+    end
+
+    it 'returns question' do
+      expect(json['question']).not_to be_empty
+    end
+
+    it 'returns question answers' do
+      expect(json['question']['answers']).not_to be_empty
+    end
   end
 
   describe 'PATCH /quizzes/{:quiz_id}/questions/{:id}' do
-    it 'return status code 200'
-    it 'returns success message'
+    let!(:updated_question) { attributes_for(:question, content: 'New content!') }
+    before { patch "/api/v1/quizzes/{:quiz_id}/questions/{:id}", headers: headers }
+
+    it 'return status code 200' do
+      expect(response).to have_http_status(200)
+    end
+
+    it 'returns success message' do
+      expect(json['message']).to match(/Question was successfully updated/)
+    end
   end
 
   describe 'DELETE /quizzes/{:quiz_id}/questions/{:id}' do
+    before { delete "/api/v1/quizzes/#{quiz.id}/questions/#{questions.last.id}" }
+
     it 'returns status code 204' do
       expect(response).to have_http_status(:no_content)
     end
